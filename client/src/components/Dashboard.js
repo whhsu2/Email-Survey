edca78bef235e91477a76cb7031b3379b221e2ec
morris754 <morris754@gmail.com>
2020-04-29 21:59:43 -0400

    user survey result

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,7 +5,6 @@ import SurveyList from './surveys/SurveyList'
 const Dashboard = () => {
     return (
         <div>
-            Dashboard
             <div className="fixed-action-btn">
                 <Link to="/surveys/new" className="btn-floating btn-large red">
                     <i className="material-icons">add</i>
